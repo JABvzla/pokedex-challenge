01a39fee3b9dc6732280ed39815b2c21aa01ec6a
Jose Bonito <joseantoniobonito@hotmail.com>
2021-01-20 01:23:54 -0300

    add font Yusei Magic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import { ThemeProvider } from "styled-components";
 import "../i18n";
 import { LightTheme } from "../theme";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
